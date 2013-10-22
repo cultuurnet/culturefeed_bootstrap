8ca752ed404befb9af2b045de4fe01189378b22f
Luk Dens <luk.dens@cultuurnet.be>
2013-10-22 17:22:01 +0200

    CFK-18 theme profile box for anonymous and logged in users

---
+++ templates/culturefeed-ui-anonymous-profile-box.tpl.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @file
+ * Template file for the culturefeed ui login box.
+ */
+
+/**
+ * @var string $link_login
+ * @var string $link_register
+ * @var string $link_login_facebook
+ */
+?>
+<ul class="menu nav navbar-nav secondary">
+  <li class="first last leaf"><?php print $link_login ?></li>
+</ul>
--- templates/culturefeed-ui-profile-box.tpl.php
+++ templates/culturefeed-ui-profile-box.tpl.php
@@ -1,23 +1,19 @@
-<ul class="nav">
-  <li class="divider-vertical"></li>
-  <?php // Render dropdown if he contains items. ?>
+<ul class="menu nav navbar-nav navbar-right">
   <?php if ($dropdown_items): ?>
   <li class="dropdown">
     <a href="#" class="dropdown-toggle" data-toggle="dropdown"><?php print $picture ?> <?php print $nick ?> <b class="caret"></b></a>
     <ul class="dropdown-menu">
       <?php foreach ($dropdown_items as $dropdown_item): ?>
-      <li class="<?php if (isset($dropdown_item['class'])): print $dropdown_item['class']?><?php endif;?> <?php if (isset($dropdown_item['children'])): print 'nav-header' ?><?php endif;?>">
+      <li class="<?php if (isset($dropdown_item['class'])): print $dropdown_item['class']?><?php endif;?> <?php if (isset($dropdown_item['children'])): print 'dropdown-header' ?><?php endif;?>">
         <?php print $dropdown_item['data']; ?>
       </li>
       <?php // Show children on same level but close with divider. ?>
       <?php if (isset($dropdown_item['children'])): ?>
-        <div class="sub-items <?php if (isset($dropdown_item['class'])): print $dropdown_item['class']?><?php endif;?>">
-          <?php foreach ($dropdown_item['children'] as $dropdown_sub_item): ?>
-          <li class="<?php if (isset($dropdown_item['class'])): print $dropdown_item['class']?><?php endif;?>">
-            <?php print $dropdown_sub_item['data']; ?>
-          </li>
-          <?php endforeach; ?>
-        </div>
+        <?php foreach ($dropdown_item['children'] as $dropdown_sub_item): ?>
+        <li class="<?php if (isset($dropdown_item['class'])): print $dropdown_item['class']?><?php endif;?>">
+          <?php print $dropdown_sub_item['data']; ?>
+        </li>
+        <?php endforeach; ?>
         <li class="divider"></li>
       <?php endif; ?>
       <?php endforeach;?>
@@ -32,14 +28,3 @@
   <?php endforeach; ?>
   <li class="divider-vertical"></li>
 </ul>
-
-<?php if (isset($no_memberships) && $no_memberships): ?>
-<div class="pull-right">
-  <?php print l($no_membership_text, 'pages/join', array('attributes' => array('class' => array('btn btn-link btn-small')))); ?>
-  <?php print l($no_membership_button, 'pages/join', array('attributes' => array('class' => array('btn btn-warning btn-small')))); ?>
-</div>
-<?php else : ?>
-<div class="pull-right">
-  <?php print l('Pagina zoeken of toevoegen', 'pages/join', array('attributes' => array('class' => array('btn btn-link btn-small')))); ?>
-</div>
-<?php endif; ?>
