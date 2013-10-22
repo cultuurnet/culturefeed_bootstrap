740f59d6a2ded95c4ea1e86db3a61dcc543907f0
Luk Dens <luk.dens@cultuurnet.be>
2013-10-22 12:58:34 +0200

    jQuery replace misc version with 1.8.3

--- template.php
+++ template.php
@@ -2,22 +2,13 @@
 
 /**
  * Implements hook_js_alter().
- */
-function culturefeed_bootstrap_js_alter(&$js) {
+*/
 
-  $path = drupal_get_path('theme', 'culturefeed_bootstrap');
-  $files = array(
-    $path . '/js/lib/jquery-1.9.1.min.js' => -101,
-    $path . '/js/jquery-switch.js' => -99,
-  );
-
-  // Place them on top of all js files.
-  foreach ($files as $file => $weight) {
-    $js[$file] = drupal_js_defaults();
-    $js[$file]['data'] = $file;
-    $js[$file]['group'] = -50;
-    $js[$file]['weight'] = $weight;
-  }
+function culturefeed_bootstrap_js_alter(&$javascript) {
+  // Replace with current version.
+  $jQuery_version = '1.8.3';
+  $javascript['misc/jquery.js']['data'] = drupal_get_path('theme', 'culturefeed_bootstrap').'/js/lib/jquery.1.8.3.min.js';
+  $javascript['misc/jquery.js']['version'] = $jQuery_version;
 }
 
 ?>
