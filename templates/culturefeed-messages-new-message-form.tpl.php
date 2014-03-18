08b4a72d9515e1feb12edce4fa39b3b1ca2ab99c
zuuperman <nils.destoop@gmail.com>
2014-03-18 17:10:55 +0100

    Rename template

--- templates/culturefeed-messages-new-message-form.tpl.php
+++ templates/culturefeed-messages-new-message-form.tpl.php
@@ -12,36 +12,36 @@ $button_printed = render($form['submit']);
 <div class="panel panel-default">
 
   <div class="panel-heading">
-  
+
     <h3 class="panel-title">
-      <?php print render($form['title']); ?> 
+      <?php print render($form['title']); ?>
     </h3>
 
   </div>
-  
+
   <div id="reply">
-  
+
     <div class="panel-body">
-  
+
       <div class="new-message-primary">
       <?php print render($form['subject'])?>
       <?php print render($form['message'])?>
       </div>
-  
+
       <div class="new-message-secondary">
         <?php print drupal_render_children($form); ?>
       </div>
-      
+
     </div>
-    
+
     <div class="panel-footer">
-    
+
        <?php
         // Render the button as last one.
         print $button_printed ?>
-    
+
     </div>
-  
+
   </div>
-  
+
 </div>
