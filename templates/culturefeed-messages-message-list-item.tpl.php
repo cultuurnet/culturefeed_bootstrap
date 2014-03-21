9fa5d5c0f5268d4fe83913969320905548274d4d
Veerle Hanssens <veerle.hanssens@cultuurnet.be>
2014-03-21 19:43:57 +0100

    CEP-259: fix for notice 'undefined var'

--- templates/culturefeed-messages-message-list-item.tpl.php
+++ templates/culturefeed-messages-message-list-item.tpl.php
@@ -1,4 +1,4 @@
-<a href="<?php print $url; ?>" class="message message-<?php print $status; ?> <?php print $class ?> list-group-item">
+<a href="<?php print $url; ?>" class="message message-<?php print $status; ?> list-group-item">
   <?php $status == 'NEW' ? print '<div class="pull-right"><i class="fa fa-envelope"></i></div>' : '' ?>
   <strong><?php print $sender; ?></strong><br />
   <?php isset($recipient_page) ? print '<small>' . t('To') . ':' . ' ' . $recipient_page . '</small><br />' : '' ; ?>
