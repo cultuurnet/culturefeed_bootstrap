1c962ab9c642bfd713a35344856528d931bddf30
Luk Dens <luk.dens@cultuurnet.be>
2014-03-21 22:35:48 +0100

    move breadcrumb in page.tpl

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -123,6 +123,8 @@
     <?php print render($page['header']); ?>
   </header> <!-- /#page-header -->
 
+  <?php if (!empty($breadcrumb)): print $breadcrumb; endif;?>
+
   <div class="row">
 
     <?php if (!empty($page['sidebar_first'])): ?>
@@ -135,7 +137,6 @@
       <?php if (!empty($page['highlighted'])): ?>
         <div class="highlighted jumbotron"><?php print render($page['highlighted']); ?></div>
       <?php endif; ?>
-      <?php if (!empty($breadcrumb)): print $breadcrumb; endif;?>
       <a id="main-content"></a>
       <?php print render($title_prefix); ?>
       <?php if (!empty($title) && !($is_front)): ?>
