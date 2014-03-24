27fcce2c2d7bf1d244084678a10bd2aed097e2f0
Stijn Swaanen <swaanenstijn@gmail.com>
2014-03-24 11:16:14 +0100

    translations CEP-516

--- templates/culturefeed-pages-request-admin-membership-block.tpl.php
+++ templates/culturefeed-pages-request-admin-membership-block.tpl.php
@@ -7,6 +7,6 @@
  */
 ?>
 <div class="well">
-  <p><strong><?php print t('Become administrator'); ?></strong></p><p> <?php print t('This page currently has no administrator. As an administrator, you can update page details, layout and manage members.'); ?></p>
+  <p><strong><?php print t('Request to become an administrator'); ?></strong></p><p> <?php print t('This page currently has no administrator. As an administrator, you can update page details, layout and manage members.'); ?></p>
 <a href="<?php print $url ?>" class="btn btn-warning btn-block"><?php print t('Send request'); ?></a>
 </div>
