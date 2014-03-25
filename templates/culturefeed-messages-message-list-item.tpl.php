39dadee1aa131ba8421df6f40370dc4da490be23
Luk Dens <luk.dens@cultuurnet.be>
2014-03-25 14:40:25 +0100

    CEP-527 finetuning theme messages inbox

--- templates/culturefeed-messages-message-list-item.tpl.php
+++ templates/culturefeed-messages-message-list-item.tpl.php
@@ -1,10 +1,18 @@
 <a href="<?php print $url; ?>" class="message message-<?php print $status; ?> <?php print $class_name ?> list-group-item">
-  <?php $status == 'NEW' ? print '<i class="fa fa-fw fa-envelope"></i> <strong>' . $subject . '</strong>' : print '<i class="fa fa-fw fa-envelope-o"></i> ' . $subject ?> <small class="text-muted">-     <?php print $date; ?></small>
-  <small class="text-muted">
-    <br />
-    <?php isset($sender) ? print t('From') . ':' . ' ' . $sender  : '' ; ?>
-    <br />
-    <?php isset($recipient_page) ? print t('To') . ':' . ' ' . $recipient_page : '' ; ?>
-  </small>
-  
+  <?php if ($status == 'NEW') : ?>
+    <i class="fa fa-fw fa-envelope text-danger pull-right"></i>
+    <small class="text-muted"><?php print $date; ?></small>
+    <h5 class="list-group-item-heading"><strong><?php print $subject; ?></strong></h5>
+  <?php else : ?>
+    <i class="fa fa-fw fa-envelope-o text-muted pull-right"></i>
+    <small class="text-muted"><?php print $date; ?></small>
+    <h5 class="list-group-item-heading"><?php print $subject; ?></h5>
+  <?php endif; ?>
+  <p class="list-group-item-text">
+    <small class="text-muted">
+      <?php isset($sender) ? print $sender  : '' ; ?>
+      <i class="fa fa-long-arrow-right"></i>
+      <?php isset($recipient_page) ? print $recipient_page : '' ; ?>
+    </small>
+  </p>
 </a>
