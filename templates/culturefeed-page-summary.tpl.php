da75ab846ea38288fcd4f76522ba6bdf65c5b5d0
Luk Dens <luk.dens@cultuurnet.be>
2014-03-26 15:09:00 +0100

    theme page summary

--- templates/culturefeed-page-summary.tpl.php
+++ templates/culturefeed-page-summary.tpl.php
@@ -18,25 +18,30 @@
   <?php endif; ?>
   
   <div class="media-body">
-    <h3 class="media-heading"><a href="<?php print $url ?>"><?php print $title; ?> <?php if (!empty($address)): ?><span class="text-muted">- <?php print $address['city']; ?><?php endif; ?></span></a></h3>
+    <div class="row">
+      <div class="col-md-9">
+        <h3 class="media-heading"><a href="<?php print $url ?>"><?php print $title; ?> <?php if (!empty($address)): ?><span class="text-muted">- <?php print $address['city']; ?><?php endif; ?></span></a></h3>
+      </div>
+      <div class="col-md-3 text-right hidden-sm hidden-xs">
+        <?php if ($follower_count > 0): ?>
+        <div class="btn-group btn-group-xs">
+        <?php print format_plural($follower_count, '<span class="btn btn-success"><strong>' . '@count' . '</strong></span><span class="btn btn-default">' .   t('follower') . '</span>' , '<span class="btn btn-success">' . '@count' . '</span><span class="btn btn-default">' .   t('followers') . '</span>'); ?>
+        </div>
+        <?php endif; ?>
+        
+        <?php if ($logged_in): ?>
     
-    <?php if ($follower_count > 0): ?>
-    <div class="btn-group btn-group-xs">
-    <?php print format_plural($follower_count, '<span class="btn btn-success"><strong>' . '@count' . '</strong></span><span class="btn btn-default">' .   t('follower') . '</span>' , '<span class="btn btn-success">' . '@count' . '</span><span class="btn btn-default">' .   t('followers') . '</span>'); ?>
+          <?php if (!$following): ?>
+            <a href="<?php print $follow_url; ?>"><small><?php print $follow_text; ?></small></a>
+          <?php else: ?>
+            <small class="text-muted"><?php print t('You follow this page'); ?></small>
+          <?php endif; ?>
+          </p>
+        <?php else: ?>
+          <p><small class="text-muted"><?php print $follow_text; ?></small></p>
+        <?php endif; ?>
+      </div>
     </div>
-    <?php endif; ?>
-    
-    <?php if ($logged_in): ?>
-
-      <?php if (!$following): ?>
-        <a href="<?php print $follow_url; ?>"><small><?php print $follow_text; ?></small></a>
-      <?php else: ?>
-        <small class="text-muted"><?php print t('You follow this page'); ?></small>
-      <?php endif; ?>
-      </p>
-    <?php else: ?>
-      <p><small class="text-muted"><?php print $follow_text; ?></small></p>
-    <?php endif; ?>
   
     <p><?php print $description ?></p>
 
