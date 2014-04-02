855ac1de638f910aaf4b0eb90cf7c2a758eb4ae8
Luk Dens <luk.dens@cultuurnet.be>
2014-04-02 12:01:58 +0200

    theme alert in admin options block

--- templates/culturefeed-pages-block-admin-options.tpl.php
+++ templates/culturefeed-pages-block-admin-options.tpl.php
@@ -35,7 +35,7 @@
 
     <?php if (!$has_activities): ?>
       <hr />
-      <div class="alert alert-warning"><?php print t('Your page currently has no published activities.'); ?> <a href="http://www.uitdatabank.be"><?php print t('Add an activity via UiTdatabank.be'); ?></a></div>
+      <div class="alert alert-warning"><small><?php print t('Your page currently has no published activities.'); ?><br /><a class="alert-link" href="http://www.uitdatabank.be"><?php print t('Add an activity via UiTdatabank.be'); ?></a></small></div>
     <?php endif; ?>
   </div>
 
