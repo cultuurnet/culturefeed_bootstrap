7370111f113ab010a46381f38136afb8e84e61ed
Luk Dens <luk.dens@cultuurnet.be>
2014-04-03 20:31:46 +0200

    theme my events page for page admins

--- templates/culturefeed-pages-events-admin-page.tpl.php
+++ templates/culturefeed-pages-events-admin-page.tpl.php
@@ -1,7 +1,7 @@
-<h3><?php print t('Activities'); ?></h3>
+<h3><?php print t('My events'); ?></h3>
  
-<?php if (empty($items)): ?>
-  <p><?php print t('The list below shows all the <strong>activities</strong> that are <strong>currently published on') . ' ' .  $page_link . ' ' . '</strong>' . t('Past or unpublished activities can be consulted via <a href="http://www.uitdatabank.be" target="_blank" class="alert-link"> UiTdatabank.be</a>.'); ?>
+<?php if (!empty($items)): ?>
+  <p><?php print t('The list below shows all the <strong>events</strong> that are <strong>currently published on') . ' ' .  $page_link . ' ' . '</strong>' . t('Past or unpublished events can be consulted via <a href="http://www.uitdatabank.be" target="_blank" class="alert-link">UiTdatabank.be</a>.'); ?>
   </p>
 
   <?php print $items; ?>
@@ -9,9 +9,15 @@
   <?php print theme('pager') ?>
 
 <?php else: ?>
-  <p>
-    <?php print t('There are <strong>currently no published activities available for')  . ' ' .  $page_link . ' ' . '</strong>.' . ' ' . t('Past or unpublished activities can be consulted via <a href="http://www.uitdatabank.be" target="_blank" class="alert-link"> UiTdatabank.be </a>.') ?>
+  <p class="alert alert-warning">
+    <?php print t('There are currently <strong>no published events</strong> for')  . ' ' .  $page_link . '.' . ' ' . t('Past or unpublished events can be consulted via <a href="http://www.uitdatabank.be" target="_blank">UiTdatabank.be</a>.') ?>
   </p>
 <?php endif; ?>
 
-<p><a class="btn btn-default" href="http://www.uitdatabank.be" target="_blank"><?php print t('Add a new activity via UiTdatabank'); ?> <i class="fa fa-external-link"></i> </a> <span class="text-muted"><?php print t('You can sign in with your current username and password.'); ?></span></p>
+<p>
+  <a class="btn btn-default" href="http://www.uitdatabank.be" target="_blank"><i class="fa fa-plus-circle"></i> <?php print t('Add new event'); ?></a> 
+</p>
+
+<p>
+  <small class="text-muted"><?php print t('You can sign in with your current username and password.'); ?></small>
+</p>
