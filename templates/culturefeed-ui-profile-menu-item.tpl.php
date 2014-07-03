5f852413cabfae0bb7d477251860cdab672b32ee
Luk Dens <luk.dens@cultuurnet.be>
2014-07-03 14:45:49 +0200

    UPS-1277 add default icon + title for profile menu items

--- templates/culturefeed-ui-profile-menu-item.tpl.php
+++ templates/culturefeed-ui-profile-menu-item.tpl.php
@@ -1,8 +1,11 @@
 <a href="/<?php print $url ?>">
   <?php
-  if (strstr($link,'gebruiker')) {
-    print '<i class="fa fa-user fa-fw fa-lg"></i> <strong>' . $title . '</strong>';
+  if (strstr($link,'user')) {
+    print '<i class="fa fa-eye fa-fw fa-lg"></i> <strong>' . $title . '</strong>';
   } 
+  elseif (strstr($link,'profile')) {
+    print '<i class="fa fa-user fa-fw fa-lg"></i> <strong>' . $title . '</strong>';
+    } 
   elseif (strstr($link,'edit')) {
     print '<i class="fa fa-pencil-square-o fa-fw fa-lg"></i> <strong>' . $title . '</strong>';
     }
@@ -10,11 +13,14 @@
     print '<i class="fa fa-group fa-fw fa-lg"></i> <strong>' . $title . '</strong>';
     }
   elseif (strstr($link,'pages')) {
-    print '<i class="fa fa-group fa-fw fa-lg"></i> <strong>' . $title . '</strong>';
+    print '<i class="fa fa-building-o fa-fw fa-lg"></i> <strong>' . $title . '</strong>';
     }
   elseif (strstr($link,'activities')) {
     print '<i class="fa fa-thumbs-up fa-fw fa-lg"></i> <strong>' . $title . '</strong>';
     } 
+  else {
+    print '<i class="fa fa-gear fa-fw fa-lg"></i> <strong>' . $title . '</strong>';
+    }
   ?>
   <br />
   <small><?php print $description ?></small>
