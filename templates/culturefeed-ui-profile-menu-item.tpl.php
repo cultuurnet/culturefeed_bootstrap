1791e2f2e2bf16d9adaf3a8b214d9e8923f2bb00
Luk Dens <luk.dens@cultuurnet.be>
2014-07-03 15:20:18 +0200

    UPS-1277 add uitpas icons to profile menu

--- templates/culturefeed-ui-profile-menu-item.tpl.php
+++ templates/culturefeed-ui-profile-menu-item.tpl.php
@@ -3,11 +3,11 @@
   if (strstr($link,'user')) {
     print '<i class="fa fa-eye fa-fw fa-lg"></i> <strong>' . $title . '</strong>';
   } 
-  elseif (strstr($link,'profile')) {
+  elseif (strstr($link,'profile') && strstr($link,'edit')) {
     print '<i class="fa fa-user fa-fw fa-lg"></i> <strong>' . $title . '</strong>';
     } 
   elseif (strstr($link,'edit')) {
-    print '<i class="fa fa-pencil-square-o fa-fw fa-lg"></i> <strong>' . $title . '</strong>';
+    print '<i class="fa fa-gear fa-fw fa-lg"></i> <strong>' . $title . '</strong>';
     }
   elseif (strstr($link,'members')) {
     print '<i class="fa fa-group fa-fw fa-lg"></i> <strong>' . $title . '</strong>';
@@ -17,6 +17,17 @@
     }
   elseif (strstr($link,'activities')) {
     print '<i class="fa fa-thumbs-up fa-fw fa-lg"></i> <strong>' . $title . '</strong>';
+    }  
+  elseif (strstr($link,'uitpas')) {  
+    if (strstr($link,'advantages')) {
+      print '<i class="fa fa-gift fa-fw fa-lg"></i> <strong>' . $title . '</strong>';
+      }  
+    elseif (strstr($link,'actions')) {
+      print '<i class="fa fa-list fa-fw fa-lg"></i> <strong>' . $title . '</strong>';
+      }   
+    elseif (strstr($link,'notifications')) {
+      print '<i class="fa fa-bell fa-fw fa-lg"></i> <strong>' . $title . '</strong>';
+      }  
     } 
   else {
     print '<i class="fa fa-gear fa-fw fa-lg"></i> <strong>' . $title . '</strong>';
