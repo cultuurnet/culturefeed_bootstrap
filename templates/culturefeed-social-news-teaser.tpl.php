7f8a751e6d66556054b36982797d4848b649182b
zuuperman <nils.destoop@gmail.com>
2014-08-04 16:54:15 +0200

    Added link on the image. Read more should be real link

--- templates/culturefeed-social-news-teaser.tpl.php
+++ templates/culturefeed-social-news-teaser.tpl.php
@@ -8,19 +8,20 @@
 <div class="media col-xs-11 well">
 
   <?php if(isset($image)): ?>
-    <img src="<?php print $image ?>" alt="<?php print $title ?>" class="media-object pull-left visible-desktop thumbnail img-responsive" width="150" />
+  <a href="<?php print $url; ?>" class="pull-left">
+    <img src="<?php print $image ?>" alt="<?php print $title ?>" class="media-object visible-desktop thumbnail img-responsive" width="150" />
+  </a>
   <?php endif; ?>
 
   <div class="media-body">
-    <h3 class="media-heading"><?php print $link; ?></h3>
+    <h3 class="media-heading"><a href="<?php print $url; ?>"><?php print $title; ?></a></h3>
 
     <?php if (!empty($summary)): ?>
     <p><?php print $summary ?></p>
 
-    <?php if (strlen($summary) != strlen($body)): ?>
-      <?php print l(t('Read more') . ' <span class="caret"></span>', '', array('attributes' => array('data-toggle' => 'collapse'), 'fragment' => 'news-full-' . $activity_id, 'html' => TRUE)) ?>
-      <div id="news-full-<?php print $activity_id ?>" class="collapse collapse-in"><?php print $body; ?></div>
-    <?php endif; ?>
+      <?php if (strlen($summary) != strlen($body)): ?>
+      <a href="<?php print $url; ?>"><?php print t('Read more'); ?></a>
+      <?php endif; ?>
     <?php endif; ?>
   </div>
 </div>
