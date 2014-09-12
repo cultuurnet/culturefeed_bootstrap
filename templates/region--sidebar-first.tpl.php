d6b97c98aa1e3011b87197c4de563ae1ec91d10b
Luk Dens <luk.dens@cultuurnet.be>
2014-09-12 10:39:19 +0200

    UIV-877 fix facets toggle for iOS

--- templates/region--sidebar-first.tpl.php
+++ templates/region--sidebar-first.tpl.php
@@ -5,12 +5,16 @@
       <div class="panel panel-default">
         <div class="panel-body">
           <div class="row visible-xs toggle-facet">
-            <div class="col-xs-12" data-toggle="collapse" data-target=".facet-container">
-              <p class="lead"><i class="fa fa-filter fa-lg"></i> Verfijn resultaten</p>
+            <div class="col-xs-12">              
+              <button data-toggle="collapse" data-target=".facet-container" class="btn btn-link btn-block">
+                <p class="lead text-left">
+                  <i class="pull-right fa fa-caret-down fa-lg"></i> Verfijn resultaten
+                </p>
+              </button>
             </div>
           </div>
           <div class="facet-container">
-          <hr class="visible-xs" />
+          <hr class="visible-xs small" />
             <?php print $content; ?>    
           </div>
         </div>
