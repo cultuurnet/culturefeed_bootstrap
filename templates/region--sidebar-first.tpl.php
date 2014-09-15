f5aa2111c6e9035b98e026a2ad0a587cf012f4a1
Luk Dens <luk.dens@cultuurnet.be>
2014-09-15 16:41:53 +0200

    theme facet collapsing element icon

--- templates/region--sidebar-first.tpl.php
+++ templates/region--sidebar-first.tpl.php
@@ -1,19 +1,19 @@
 <?php if ($content): ?>
-  <div class="region region-sidebar-first">
+  <div class="region region-sidebar-first" id="facet-wrapper">
 
     <?php if ($pagetype == 'agenda-search') : ?>
       <div class="panel panel-default">
         <div class="panel-body">
           <div class="row visible-xs toggle-facet">
             <div class="col-xs-12">              
-              <button data-toggle="collapse" data-target=".facet-container" class="btn btn-link btn-block">
+              <button data-toggle="collapse" data-target="#facet-container" data-parent="#facet-wrapper" class="btn btn-link btn-block collapsed">
                 <p class="lead text-left">
-                  <i class="pull-right fa fa-caret-down fa-lg"></i> Verfijn resultaten
+                  <i class="pull-right fa"></i> Verfijn of wijzig resultaten
                 </p>
               </button>
             </div>
           </div>
-          <div class="facet-container">
+          <div id="facet-container">
           <hr class="visible-xs small" />
             <?php print $content; ?>    
           </div>
