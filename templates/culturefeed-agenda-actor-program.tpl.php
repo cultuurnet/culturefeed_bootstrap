6c0d1277c16a63702c23f25075de67ca570d50a9
Luk Dens <luk.dens@cultuurnet.be>
2014-09-16 17:23:50 +0200

    theme actor program block

--- templates/culturefeed-agenda-actor-program.tpl.php
+++ templates/culturefeed-agenda-actor-program.tpl.php
@@ -5,7 +5,6 @@
  */
 ?>
 
-
 <?php if (!empty($items)): ?>
   <h2><i class="fa fa-calendar 2-x"></i> <?php print t('Calendar'); ?></h2>
   <ul class="list-unstyled">
@@ -13,9 +12,8 @@
   <li><?php print $item ?></li>
   <?php endforeach;?>
   </ul>
-  <p><a href="<?php print $search_url ?>" class="btn btn-default"><?php print t('More activities'); ?> →</a></p>
+  <p><a href="<?php print $search_url ?>" class="btn btn-default"><?php print t('Show full calendar of') . ' ' . $title; ?> →</a></p>
 <?php else: ?>
   <h3><i class="fa fa-calendar-o 2-x"></i> <?php print t('Calendar'); ?></h3>
-  <p class="muted"><?php print t('There are currently no activities available.'); ?></p>
+  <p class="text-muted"><?php print t('There are currently no activities available.'); ?></p>
 <?php endif; ?> 
-
