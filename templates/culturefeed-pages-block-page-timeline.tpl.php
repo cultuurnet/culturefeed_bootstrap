0ad1284df3c3b26c78d09a8cd4c6257985c1d532
Luk Dens <luk.dens@cultuurnet.be>
2014-09-19 13:40:37 +0200

    theme button add news item on page timeline

--- templates/culturefeed-pages-block-page-timeline.tpl.php
+++ templates/culturefeed-pages-block-page-timeline.tpl.php
@@ -7,9 +7,7 @@
 ?>
 
 <?php if ($page_admin): ?>
-<p>
-    <a href="<?php print url('pages/' . $page_id . '/news/add'); ?>" class="btn btn-default"><?php print t('Add a news item') ?> &rarr;</a>
-</p>
+  <a href="<?php print url('pages/' . $page_id . '/news/add'); ?>" class="btn btn-warning btn-default pull-left"><?php print t('Add a news item') ?> &rarr;</a>
 <?php endif; ?>
 
 <?php print render($filter_form); ?>
