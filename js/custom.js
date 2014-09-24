3f658aa9c2ab30cf4b203814552a89679c63784b
zuuperman <nils.destoop@gmail.com>
2014-09-24 12:31:33 +0200

    WKS-241: Don't show an error code when navigating away

--- js/custom.js
+++ js/custom.js
@@ -141,4 +141,85 @@ jQuery(function($) {
     }
   }
 
+  /**
+   * Handler for the form redirection error.
+   * Custom override: Don't show an error when people are navigation away of the site.
+   */
+  Drupal.ajax.prototype.error = function (response, uri) {
+
+    if (!response.status) {
+      return;
+    }
+
+    alert(Drupal.ajaxError(response, uri));
+    // Remove the progress element.
+    if (this.progress.element) {
+      $(this.progress.element).remove();
+    }
+    if (this.progress.object) {
+      this.progress.object.stopMonitoring();
+    }
+    // Undo hide.
+    $(this.wrapper).show();
+    // Re-enable the element.
+    $(this.element).removeClass('progress-disabled').removeAttr('disabled');
+    // Reattach behaviors, if they were detached in beforeSerialize().
+    if (this.form) {
+      var settings = response.settings || this.settings || Drupal.settings;
+      Drupal.attachBehaviors(this.form, settings);
+    }
+  };
+
+  /**
+   * Performs a cached and delayed search.
+   * Custom override: Don't show an error when people are navigation away of the site.
+   */
+  Drupal.ACDB.prototype.search = function (searchString) {
+    var db = this;
+    this.searchString = searchString;
+
+    // See if this string needs to be searched for anyway.
+    searchString = searchString.replace(/^\s+|\s+$/, '');
+    if (searchString.length <= 0 ||
+      searchString.charAt(searchString.length - 1) == ',') {
+      return;
+    }
+
+    // See if this key has been searched for before.
+    if (this.cache[searchString]) {
+      return this.owner.found(this.cache[searchString]);
+    }
+
+    // Initiate delayed search.
+    if (this.timer) {
+      clearTimeout(this.timer);
+    }
+    this.timer = setTimeout(function () {
+      db.owner.setStatus('begin');
+
+      // Ajax GET request for autocompletion. We use Drupal.encodePath instead of
+      // encodeURIComponent to allow autocomplete search terms to contain slashes.
+      $.ajax({
+        type: 'GET',
+        url: db.uri + '/' + Drupal.encodePath(searchString),
+        dataType: 'json',
+        success: function (matches) {
+          if (typeof matches.status == 'undefined' || matches.status != 0) {
+            db.cache[searchString] = matches;
+            // Verify if these are still the matches the user wants to see.
+            if (db.searchString == searchString) {
+              db.owner.found(matches);
+            }
+            db.owner.setStatus('found');
+          }
+        },
+        error: function (xmlhttp) {
+          if (xmlhttp.status) {
+            alert(Drupal.ajaxError(xmlhttp, db.uri));
+          }
+        }
+      });
+    }, this.delay);
+  };
+
 });
