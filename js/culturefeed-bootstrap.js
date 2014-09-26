b2034c1c5d850f9100dcee7f7b6919385f58ed3d
zuuperman <nils.destoop@gmail.com>
2014-09-26 14:37:04 +0200

    Add checks if main object exists

--- js/culturefeed-bootstrap.js
+++ js/culturefeed-bootstrap.js
@@ -110,118 +110,128 @@ jQuery(function($) {
     }
   };
 
-  Drupal.file = Drupal.file || {};
+  if (Drupal.file) {
 
-  /**
-   * Client-side file input validation of file extensions.
-   */
-  Drupal.file.validateExtension = function (event) {
+    /**
+     * Client-side file input validation of file extensions.
+     */
+    Drupal.file.validateExtension = function (event) {
 
-    // Remove any previous errors.
-    $('.file-upload-js-error').remove();
+      // Remove any previous errors.
+      $('.file-upload-js-error').remove();
 
-    // Add client side validation for the input[type=file].
-    var extensionPattern = event.data.extensions.replace(/,\s*/g, '|');
-    if (extensionPattern.length > 1 && this.value.length > 0) {
-      var acceptableMatch = new RegExp('\\.(' + extensionPattern + ')$', 'gi');
-      if (!acceptableMatch.test(this.value)) {
-        var error = Drupal.t("The selected file %filename cannot be uploaded. Only files with the following extensions are allowed: %extensions.", {
-          // According to the specifications of HTML5, a file upload control
-          // should not reveal the real local path to the file that a user
-          // has selected. Some web browsers implement this restriction by
-          // replacing the local path with "C:\fakepath\", which can cause
-          // confusion by leaving the user thinking perhaps Drupal could not
-          // find the file because it messed up the file path. To avoid this
-          // confusion, therefore, we strip out the bogus fakepath string.
-          '%filename': this.value.replace('C:\\fakepath\\', ''),
-          '%extensions': extensionPattern.replace(/\|/g, ', ')
-        });
-        $(this).closest('div.form-type-managed-file').append('<div class="messages error file-upload-js-error">' + error + '</div>');
-        this.value = '';
-        return false;
+      // Add client side validation for the input[type=file].
+      var extensionPattern = event.data.extensions.replace(/,\s*/g, '|');
+      if (extensionPattern.length > 1 && this.value.length > 0) {
+        var acceptableMatch = new RegExp('\\.(' + extensionPattern + ')$', 'gi');
+        if (!acceptableMatch.test(this.value)) {
+          var error = Drupal.t("The selected file %filename cannot be uploaded. Only files with the following extensions are allowed: %extensions.", {
+            // According to the specifications of HTML5, a file upload control
+            // should not reveal the real local path to the file that a user
+            // has selected. Some web browsers implement this restriction by
+            // replacing the local path with "C:\fakepath\", which can cause
+            // confusion by leaving the user thinking perhaps Drupal could not
+            // find the file because it messed up the file path. To avoid this
+            // confusion, therefore, we strip out the bogus fakepath string.
+            '%filename': this.value.replace('C:\\fakepath\\', ''),
+            '%extensions': extensionPattern.replace(/\|/g, ', ')
+          });
+          $(this).closest('div.form-type-managed-file').append('<div class="messages error file-upload-js-error">' + error + '</div>');
+          this.value = '';
+          return false;
+        }
       }
     }
+
   }
 
-  /**
-   * Handler for the form redirection error.
-   * Custom override: Don't show an error when people are navigation away of the site.
-   */
-  Drupal.ajax.prototype.error = function (response, uri) {
+  if (Drupal.ajax) {
 
-    if (!response.status) {
-      return;
-    }
+    /**
+     * Handler for the form redirection error.
+     * Custom override: Don't show an error when people are navigation away of the site.
+     */
+    Drupal.ajax.prototype.error = function (response, uri) {
 
-    alert(Drupal.ajaxError(response, uri));
-    // Remove the progress element.
-    if (this.progress.element) {
-      $(this.progress.element).remove();
-    }
-    if (this.progress.object) {
-      this.progress.object.stopMonitoring();
-    }
-    // Undo hide.
-    $(this.wrapper).show();
-    // Re-enable the element.
-    $(this.element).removeClass('progress-disabled').removeAttr('disabled');
-    // Reattach behaviors, if they were detached in beforeSerialize().
-    if (this.form) {
-      var settings = response.settings || this.settings || Drupal.settings;
-      Drupal.attachBehaviors(this.form, settings);
-    }
-  };
+      if (!response.status) {
+        return;
+      }
 
-  /**
-   * Performs a cached and delayed search.
-   * Custom override: Don't show an error when people are navigation away of the site.
-   */
-  Drupal.ACDB.prototype.search = function (searchString) {
-    var db = this;
-    this.searchString = searchString;
+      alert(Drupal.ajaxError(response, uri));
+      // Remove the progress element.
+      if (this.progress.element) {
+        $(this.progress.element).remove();
+      }
+      if (this.progress.object) {
+        this.progress.object.stopMonitoring();
+      }
+      // Undo hide.
+      $(this.wrapper).show();
+      // Re-enable the element.
+      $(this.element).removeClass('progress-disabled').removeAttr('disabled');
+      // Reattach behaviors, if they were detached in beforeSerialize().
+      if (this.form) {
+        var settings = response.settings || this.settings || Drupal.settings;
+        Drupal.attachBehaviors(this.form, settings);
+      }
+    };
 
-    // See if this string needs to be searched for anyway.
-    searchString = searchString.replace(/^\s+|\s+$/, '');
-    if (searchString.length <= 0 ||
-      searchString.charAt(searchString.length - 1) == ',') {
-      return;
-    }
+  }
 
-    // See if this key has been searched for before.
-    if (this.cache[searchString]) {
-      return this.owner.found(this.cache[searchString]);
-    }
+  if (Drupal.ACDB) {
 
-    // Initiate delayed search.
-    if (this.timer) {
-      clearTimeout(this.timer);
-    }
-    this.timer = setTimeout(function () {
-      db.owner.setStatus('begin');
+    /**
+     * Performs a cached and delayed search.
+     * Custom override: Don't show an error when people are navigation away of the site.
+     */
+    Drupal.ACDB.prototype.search = function (searchString) {
+      var db = this;
+      this.searchString = searchString;
 
-      // Ajax GET request for autocompletion. We use Drupal.encodePath instead of
-      // encodeURIComponent to allow autocomplete search terms to contain slashes.
-      $.ajax({
-        type: 'GET',
-        url: db.uri + '/' + Drupal.encodePath(searchString),
-        dataType: 'json',
-        success: function (matches) {
-          if (typeof matches.status == 'undefined' || matches.status != 0) {
-            db.cache[searchString] = matches;
-            // Verify if these are still the matches the user wants to see.
-            if (db.searchString == searchString) {
-              db.owner.found(matches);
+      // See if this string needs to be searched for anyway.
+      searchString = searchString.replace(/^\s+|\s+$/, '');
+      if (searchString.length <= 0 ||
+        searchString.charAt(searchString.length - 1) == ',') {
+        return;
+      }
+
+      // See if this key has been searched for before.
+      if (this.cache[searchString]) {
+        return this.owner.found(this.cache[searchString]);
+      }
+
+      // Initiate delayed search.
+      if (this.timer) {
+        clearTimeout(this.timer);
+      }
+      this.timer = setTimeout(function () {
+        db.owner.setStatus('begin');
+
+        // Ajax GET request for autocompletion. We use Drupal.encodePath instead of
+        // encodeURIComponent to allow autocomplete search terms to contain slashes.
+        $.ajax({
+          type: 'GET',
+          url: db.uri + '/' + Drupal.encodePath(searchString),
+          dataType: 'json',
+          success: function (matches) {
+            if (typeof matches.status == 'undefined' || matches.status != 0) {
+              db.cache[searchString] = matches;
+              // Verify if these are still the matches the user wants to see.
+              if (db.searchString == searchString) {
+                db.owner.found(matches);
+              }
+              db.owner.setStatus('found');
             }
-            db.owner.setStatus('found');
+          },
+          error: function (xmlhttp) {
+            if (xmlhttp.status) {
+              alert(Drupal.ajaxError(xmlhttp, db.uri));
+            }
           }
-        },
-        error: function (xmlhttp) {
-          if (xmlhttp.status) {
-            alert(Drupal.ajaxError(xmlhttp, db.uri));
-          }
-        }
-      });
-    }, this.delay);
-  };
+        });
+      }, this.delay);
+    };
+
+  }
 
 });
