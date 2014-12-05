ae18e119f10b852a12f0bdc665a5b0495dd15647
zuuperman <nils.destoop@gmail.com>
2014-12-05 15:54:20 +0100

    added button

---
+++ templates/culturefeed-calendar-button.tpl.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @file
+ * Template for the calendar add or view buttons.
+ */
+?>
+
+<div class="calendar-button">
+  <?php if (isset($button['description'])) : ?>
+    <?php print $button['description']; ?>
+  <?php endif; ?>
+
+  <a href="<?php print $url; ?>" data-toggle="modal" data-target="#modal-calendar" data-remote="<?php print $url; ?>/ajax"><?php print $button['text']; ?></a>
+</div>
+
+<div id="modal-calendar" class="modal hide fade" tabindex="-1" role="dialog" aria-hidden="true">
+  <div class="modal-body outer"></div>
+</div>'
