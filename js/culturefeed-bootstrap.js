35483a400b781fedd774f4299a973ee58547f114
zuuperman <nils.destoop@gmail.com>
2014-12-09 17:50:41 +0100

    Add code to a document.ready

--- js/culturefeed-bootstrap.js
+++ js/culturefeed-bootstrap.js
@@ -1,60 +1,75 @@
 (function ($) {
 
-  // Init popovers
-  $("a[data-toggle=popover]")
-    .popover()
-    .click(function(e) {
-      e.preventDefault()
-    })
+  $(document).ready(function() {
 
-  // popover - authentication required
-  if (!Drupal.settings.culturefeed || !Drupal.settings.culturefeed.isCultureFeedUser) {
+    // Init popovers
+    $("a[data-toggle=popover]")
+      .popover()
+      .click(function(e) {
+        e.preventDefault()
+      })
 
-    var isVisible = false;
-    var clickedAway = false;
+    // popover - authentication required
+    if (!Drupal.settings.culturefeed || !Drupal.settings.culturefeed.isCultureFeedUser) {
 
-    $popoverLogin = $(".do-link");
+      var isVisible = false;
+      var clickedAway = false;
 
-    // show popover
-    $popoverLogin.popover({
-      html: true,
-      placement: 'top',
-      trigger: 'manual',
-      content: function() {
-        // see custom culturefeed-ui-connect-hover.tpl.php
-        return $(".popover-login").html();
-      }
-    }).click(function(e) {
-      $(this).popover('show');
-      clickedAway = false
-      isVisible = true
-      e.preventDefault()
+      $popoverLogin = $(".do-link");
+
+      // show popover
+      $popoverLogin.popover({
+        html: true,
+        placement: 'top',
+        trigger: 'manual',
+        content: function() {
+          // see custom culturefeed-ui-connect-hover.tpl.php
+          return $(".popover-login").html();
+        }
+      }).click(function(e) {
+        $(this).popover('show');
+        clickedAway = false
+        isVisible = true
+        e.preventDefault()
+      });
+
+      // hide popover
+      $(document).click(function(e) {
+        if (isVisible && clickedAway)
+        {
+          $popoverLogin.popover('hide')
+          isVisible = clickedAway = false
+        }
+        else
+        {
+          clickedAway = true
+        }
+      });
+
+    }
+
+    // Init tooltips
+    $("a[data-toggle=tooltip]").tooltip();
+    $("span[data-toggle=tooltip]").tooltip();
+
+    // Init the map if this toggles a map.
+    $(".map-toggle").click(function() {
+      Drupal.CultureFeed.Agenda.initializeMap();
     });
 
-
-    // hide popover
-    $(document).click(function(e) {
-      if (isVisible && clickedAway)
-      {
-        $popoverLogin.popover('hide')
-        isVisible = clickedAway = false
-      }
-      else
-      {
-        clickedAway = true
-      }
+    // Count characters input field - limit 400 characters
+    $(function(){
+          $('#limit-400').keyup(function(){
+                  limitChars('limit-400', 400, 'charlimitinfo');
+          })
     });
 
-  }
+    // Remove agenda tab from page timeline block if agenda block is not available
+    if ($("#block-culturefeed-pages-page-agenda").length == 0) {
+      $(".tab-agenda").remove();
+    }
 
-  // Init tooltips
-  $("a[data-toggle=tooltip]").tooltip();
-  $("span[data-toggle=tooltip]").tooltip();
-
-  // Init the map if this toggles a map.
-  $(".map-toggle").click(function() {
-    Drupal.CultureFeed.Agenda.initializeMap();
-  });
+  })
 
   // Count characters input field - limit - general
   function limitChars(textid, limit, infodiv)
@@ -73,18 +88,6 @@
   		$('#' + infodiv).html('Nog '+ (limit - textlength) +' resterende karakters');
   		return true;
   	}
-  }
-
-  // Count characters input field - limit 400 characters
-  $(function(){
-   	$('#limit-400').keyup(function(){
-   		limitChars('limit-400', 400, 'charlimitinfo');
-   	})
-  });
-
-  // Remove agenda tab from page timeline block if agenda block is not available
-  if ($("#block-culturefeed-pages-page-agenda").length == 0) {
-    $(".tab-agenda").remove();
   }
 
   /**
