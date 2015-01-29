b250c72825b0c39bc5624bd997e921624800e7e5
zuuperman <nils.destoop@gmail.com>
2015-01-29 14:27:02 +0100

    UIT-13: Make css / js more general for better support between themes

--- templates/culturefeed-calendar-button.tpl.php
+++ templates/culturefeed-calendar-button.tpl.php
@@ -2,36 +2,29 @@
 /**
  * @file
  * Template for the calendar add or view buttons.
- * Leave the wrapper div class + data-eventid, as it is used for javascript.
+ * Leave the wrapper div class + print classes + data-eventid, as it is used for javascript.
  */
-$classes = implode(' ', $button['options']['attributes']['class']);
 ?>
 
-<div class="calendar-button" data-eventid="<?php print $event_id; ?>">
+<div class="<?php print $classes; ?>" data-eventid="<?php print $event_id; ?>">
 
 <?php if ($button['action'] == 'view') : ?>
   <?php if ($button['location'] == 'content') : ?>
-    <div class="<?php print $classes; ?>">
-      <span><?php print t('This event is added to your calendar.'); ?></span>
-      <a href="<?php print url($button['path']); ?>"><?php print $button['text']; ?></a>
-    </div>
+  <span><?php print t('This event is added to your calendar.'); ?></span>
+  <a href="<?php print url($button['path']); ?>"><?php print $button['text']; ?></a>
   <?php elseif ($button['location'] == 'footer') : ?>
-    <div class="<?php print $classes; ?>">
-      <span>
-        <?php print t('This event has been saved'); ?>
-      </span>
-      <a href="<?php print url($button['path']); ?>"><?php print $button['text']; ?></a>
-      <?php if (!$authenticated) : ?>
-        <span>
-          <?php print t("Don't forget to login to save your events to your calendar"); ?>
-        </span>
-      <?php endif; ?>
-    </div>
+  <span>
+    <?php print t('This event has been saved'); ?>
+  </span>
+  <a href="<?php print url($button['path']); ?>"><?php print $button['text']; ?></a>
+  <?php if (!$authenticated) : ?>
+    <span>
+      <?php print t("Don't forget to login to save your events to your calendar"); ?>
+    </span>
+  <?php endif; ?>
   <?php endif; ?>
 <?php elseif ($button['action'] == 'add') : ?>
-  <div class="<?php print $classes . ' ' . $classes . '-' . $button['location']; ?>">
-    <a href="<?php print url($button['path'], $button['options']); ?>" class="use-ajax"><?php print $button['text']; ?></a>
-  </div>
+  <a href="<?php print url($button['path'], $button['options']); ?>" class="use-ajax"><?php print $button['text']; ?></a>
 <?php endif; ?>
 
 </div>
