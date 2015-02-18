91499542a22f0c22e8186258f9a24608856a83f4
Luk Dens <luk.dens@cultuurnet.be>
2015-02-18 22:01:47 +0100

    theme calendar events list

--- templates/culturefeed-calendar-activities-going.tpl.php
+++ templates/culturefeed-calendar-activities-going.tpl.php
@@ -12,9 +12,11 @@
       <div class="calendar-activity-wrapper col-xs-12">
         <h4 id="<?php print drupal_strtolower($month_name); ?>" class="calendar-activities-label"><?php print $month_name ?></h4>
         <?php if (!empty($activities)): ?>
+        <div class="row">
           <?php foreach ($activities as $activity): ?>
             <?php print theme('culturefeed_calendar_activity_summary', array('activity' => $activity, 'my_calendar' => $my_calendar)) ?>
           <?php endforeach; ?>
+        </div>
         <?php else: ?>
           <p class="small text-muted"><?php print t('No events scheduled in this month'); ?></p>
         <?php endif; ?>
