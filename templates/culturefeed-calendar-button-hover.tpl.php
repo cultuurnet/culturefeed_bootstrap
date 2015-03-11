1582db7e398ccb3c3e7b82dfaa7df6e182ee7b6d
Luk Dens <luk.dens@cultuurnet.be>
2015-03-11 13:35:48 +0100

    theme calendar item in profile box

--- templates/culturefeed-calendar-button-hover.tpl.php
+++ templates/culturefeed-calendar-button-hover.tpl.php
@@ -1,12 +1,5 @@
 <div class="tooltip-login hover">
-  <p><?php print t('You have temporarily added events to your calendar. Log in and save the events to you personal calendar.') ?></p>
-  <hr class="small" />
-  <div class="row">
-    <div class="col-lg-6">
-      <?php print l(t('Log in'), $url, $options) ?>
-    </div>
-    <div class="col-lg-6">
-      <?php print l(t('View calendar'), 'culturefeed/calendar', array('attributes' => array('class' => array('', 'btn-link btn-block text-center')))) ?>
-    </div>
-  </div>
+  <p class="small text-muted"><?php print t('You have temporarily added events to your calendar. Log in and save the events to your personal calendar.') ?></p>
+  <?php print l(t('Log in'), $url, $options) ?>
+  <?php print l(t('View calendar'), 'culturefeed/calendar', array('attributes' => array('class' => array('btn', 'btn-link', 'btn-block', 'text-center')))) ?>
 </div>
