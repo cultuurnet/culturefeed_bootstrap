771d7fc1ac84619306b7da6c97caa16576942600
Reinout Bossuyt <reinout@cultuurnet.be>
2015-03-20 13:47:11 +0100

    Theming reservation-button

--- template.php
+++ template.php
@@ -257,9 +257,10 @@ function _culturefeed_bootstrap_preprocess_culturefeed_agenda_detail(&$variables
 
   // Ticket buttons.
   if (isset($variables['ticket_buttons']) && !empty($variables['ticket_buttons'])) {
+
     $buttons = $variables['ticket_buttons'];
     foreach ($buttons as $button) {
-      $ticket_button[] = l($button['text'], $button['link'], array('attributes' => array('class' => 'btn btn-warning btn-xs reservation-link', 'id' => 'cf-ticket'), 'html' => TRUE));
+      $ticket_button[] = l($button['text'], $button['link'], array('attributes' => array('class' => 'btn btn-primary reservation-link', 'id' => 'cf-ticket'), 'html' => TRUE));
     }
     $variables['ticket_buttons'] = implode(' ', $ticket_button);
   }
