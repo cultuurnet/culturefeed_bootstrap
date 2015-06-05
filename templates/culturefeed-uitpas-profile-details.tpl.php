2357a2e8397a03892f76c8217a64e81de9f9709e
HnLn <hans.langouche@gmail.com>
2015-06-05 12:21:14 +0200

    MSS-184 Aanpassingen voor uitpas profile details

--- templates/culturefeed-uitpas-profile-details.tpl.php
+++ templates/culturefeed-uitpas-profile-details.tpl.php
@@ -32,6 +32,11 @@
 
 <hr />
 
+<?php if ($intro): ?>
+  <p class="intro"><?php print $intro; ?></p>
+<?php endif; ?>
+
+
 <div class="panel-group profile uitpas" id="accordion">
   <div class="panel panel-default">
     <div class="panel-heading">
@@ -52,31 +57,17 @@
     <div class="panel-heading">
       <h3 class="panel-title">
         <a data-toggle="collapse" data-parent="#accordion" href="#personalData">
-          <i class="fa fa-caret-down"></i> <?php print(t('My personal data')) ?>  
+          <i class="fa fa-caret-down"></i> <?php print $form_title; ?>
         </a>
       </h3>
     </div>  
     <div id="personalData" class="panel-collapse collapse">
       <div class="panel-body">  
       
-          <?php if ($intro): ?>
-          <p><?php print $intro; ?></p>
+          <?php if ($form_intro): ?>
+          <div class="form-intro"><?php print $form_intro; ?></div>
           <?php endif; ?>
-        
-          <ul class="data">
-            <li><?php print $first_name; ?></li>
-            <li><?php print $last_name; ?></li>
-            <li><?php print $dob; ?></li>
-            <li><?php print $pob; ?></li>
-            <li><?php print $gender; ?></li>
-            <li><?php print $nationality; ?></li>
-            <li><?php print $street; ?></li>
-            <li><?php print $zip; ?></li>
-            <li><?php print $city; ?></li>
-            <li><?php print $tel; ?></li>
-            <li><?php print $mobile; ?></li>
-            <li><?php print $email; ?></li>
-          </ul>
+          <?php print $form; ?>
 
           <?php if ($outro): ?>
           <p><?php print $outro; ?></p>
