f9468ce06722e04e82923f50f6c4d6524d46e611
HnLn <hans.langouche@gmail.com>
2015-06-15 16:08:27 +0200

    MSS-177 remove accordion

--- templates/culturefeed-ui-page-account-edit.tpl.php
+++ templates/culturefeed-ui-page-account-edit.tpl.php
@@ -16,37 +16,16 @@
 
 <div id="account-edit-form">
   <?php print $form ?>
-</div><hr />
+</div>
+<hr />
 
-<div class="panel-group" id="accordion">
-  <div class="panel panel-default">
-    <div class="panel-heading">
-      <h4 class="panel-title">
-        <span class="caret"></span>
-        <a data-toggle="collapse" data-parent="#accordion" href="#online_accounts">
-          <?php print t('Connected accounts'); ?>
-        </a>
-      </h4>
-    </div>
-    <div id="online_accounts" class="panel-collapse collapse">
-      <div class="panel-body">
-        <?php print $online_accounts ?>
-      </div>
-    </div>
-  </div>
-  <div class="panel panel-default">
-    <div class="panel-heading">
-      <h4 class="panel-title">
-        <span class="caret"></span>
-        <a data-toggle="collapse" data-parent="#accordion" href="#manage-consumers">
-          <?php print t('Connected applications'); ?>
-        </a>
-      </h4>
-    </div>
-    <div id="manage-consumers" class="panel-collapse collapse">
-      <div class="panel-body">
-        <p><?php print $connected_applications; ?></p>
-      </div>
-    </div>
-  </div>
+<h4><?php print t('Connected accounts'); ?></h4>
+<div id="online_accounts">
+  <?php print $online_accounts ?>
+</div>
+<hr />
+
+<h4><?php print t('Connected applications'); ?></h4>
+<div id="manage-consumers">
+  <p><?php print $connected_applications; ?></p>
 </div>
