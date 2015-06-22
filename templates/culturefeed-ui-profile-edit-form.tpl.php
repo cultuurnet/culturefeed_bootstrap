02d6b33d4354bee3108323aaf3989552aabbfe52
Julie Vanopdenbosch <julievanopdenbosch@Julies-MacBook-Air.local>
2015-06-22 19:17:00 +0200

    MSS-176 UiV3: Aanpassen persoonsgegevens formulier fintune responsive fields

--- templates/culturefeed-ui-profile-edit-form.tpl.php
+++ templates/culturefeed-ui-profile-edit-form.tpl.php
@@ -8,12 +8,12 @@
 ?>
 
 <div class="row">
-  <div class="col-xs-6"><?php print $givenName; ?></div>
-  <div class="col-xs-6"><?php print $familyName; ?></div>
+  <div class="col-sm-6"><?php print $givenName; ?></div>
+  <div class="col-sm-6"><?php print $familyName; ?></div>
 </div>
 <div class="row">
-  <div class="col-xs-6"><?php print $dob; ?></div>
-  <div class="col-xs-6"><?php print $gender; ?></div>
+  <div class="col-sm-6"><?php print $dob; ?></div>
+  <div class="col-sm-6"><?php print $gender; ?></div>
 </div>
   <?php print $picture; ?>
 <hr />
@@ -21,8 +21,8 @@
   <div class="col-xs-12"><?php print $street; ?></div>
 </div>
 <div class="row">
-  <div class="col-xs-3"><?php print $zip; ?></div>
-  <div class="col-xs-9"><?php print $city; ?></div>
+  <div class="col-sm-3"><?php print $zip; ?></div>
+  <div class="col-sm-9"><?php print $city; ?></div>
 </div>
 <div class="row">
   <div class="col-xs-6"><?php print $country; ?></div>
