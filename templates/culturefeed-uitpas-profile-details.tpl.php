51f8ee061e7fe268e3d608f803af0365188b4fa7
HnLn <hans.langouche@gmail.com>
2015-06-22 13:09:59 +0200

    MSS-184 move theming to bootstrap

--- templates/culturefeed-uitpas-profile-details.tpl.php
+++ templates/culturefeed-uitpas-profile-details.tpl.php
@@ -30,75 +30,38 @@
  */
 ?>
 
-<hr />
-
 <?php if ($intro): ?>
   <p class="intro"><?php print $intro; ?></p>
 <?php endif; ?>
 
+<h2><?php print $uitpas_title; ?> </h2>
+<div class="uitpas_numbers"><?php print $uitpas_numbers; ?></div>
 
-<div class="panel-group profile uitpas" id="accordion">
-  <div class="panel panel-default">
-    <div class="panel-heading">
-      <h3 class="panel-title">
-        <a data-toggle="collapse" data-parent="#accordion" href="#uitpas">
-          <i class="fa fa-caret-down"></i> <?php print $uitpas_title; ?>  
-        </a>
-      </h3>
-    </div>
-    <div id="uitpas" class="panel-collapse collapse in">
-      <div class="panel-body">
-          <div class="uitpas_numbers"><?php print $uitpas_numbers; ?></div>
-      </div>
-    </div>
-  </div>
-  
-  <div class="panel panel-default">
-    <div class="panel-heading">
-      <h3 class="panel-title">
-        <a data-toggle="collapse" data-parent="#accordion" href="#personalData">
-          <i class="fa fa-caret-down"></i> <?php print $form_title; ?>
-        </a>
-      </h3>
-    </div>  
-    <div id="personalData" class="panel-collapse collapse">
-      <div class="panel-body">  
-      
-          <?php if ($form_intro): ?>
-          <div class="form-intro"><?php print $form_intro; ?></div>
-          <?php endif; ?>
-          <?php print $form; ?>
+<h3><?php print $form_title; ?></h3>
+<div id="personalData">
 
-          <?php if ($outro): ?>
-          <p><?php print $outro; ?></p>
-          <?php endif; ?>
+  <?php if ($form_intro): ?>
+  <div class="form-intro"><?php print $form_intro; ?></div>
+  <?php endif; ?>
 
-      </div>
-    </div>
-  </div>
-  
-  <?php if ($kansen_statuut && $kansen_statuut_valid_end_date): ?>
-  <div class="panel panel-default">
-    <div class="panel-heading">
-      <h3 class="panel-title">
-        <a data-toggle="collapse" data-parent="#accordion" href="#opportunitiesTariff">
-          <i class="fa fa-caret-down"></i> <?php print(t('My opportunities tariff')) ?>  
-        </a>
-      </h3>
-    </div>  
-    <div id="#opportunitiesTariff" class="panel-collapse collapse">
-      <div class="panel-body">    
-          <div class="status">
-            <h3><?php print $status_title; ?></h3>
-            <p><?php print $status_valid_till; ?></p>
-            <?php if ($memberships): ?>
-            <p><?php print $memberships; ?></p>
-            <?php endif; ?>
-          </div>
-      </div>
-    </div>
-  </div>
+  <?php print $form; ?>
+
+  <?php if ($outro): ?>
+  <p><?php print $outro; ?></p>
   <?php endif; ?>
 
 </div>
+
+<?php if ($kansen_statuut && $kansen_statuut_valid_end_date): ?>
+<h3><?php print(t('My opportunities tariff')) ?></h3>
+<div id="#opportunitiesTariff">
+  <div class="status">
+    <h4><?php print $status_title; ?></h4>
+    <p><?php print $status_valid_till; ?></p>
+    <?php if ($memberships): ?>
+    <p><?php print $memberships; ?></p>
+    <?php endif; ?>
+  </div>
+</div>
+<?php endif; ?>
 
