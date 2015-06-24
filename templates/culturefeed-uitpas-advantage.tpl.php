1dd7b0030987927265229b955cab07de765a6d82
Julie Vanopdenbosch <julievanopdenbosch@Julies-MacBook-Air.local>
2015-06-24 11:17:41 +0200

    MSS-176 UiV3: Aanpassen persoonsgegevens formulier & MSS-187 UiV3: Aanpassen UiTpas voordeel detailpagina

--- templates/culturefeed-uitpas-advantage.tpl.php
+++ templates/culturefeed-uitpas-advantage.tpl.php
@@ -30,7 +30,7 @@
     <p class="description1"><?php print $description1; ?></p>
     <?php endif; ?>
       <div class="how-to-exchange">
-        <button class="show-exchange-info" onclick="Drupal.CultureFeed.UiTPASToggleExchangeInfo()"><?php print t('How to exchange'); ?></button>
+        <button class="show-exchange-info btn btn-primary" onclick="Drupal.CultureFeed.UiTPASToggleExchangeInfo()"><?php print t('How to exchange'); ?></button>
         <div class="exchange-info">
           <div class="locations">
             <?php print t('At') . ' ' . implode(', ', $location_links); ?>
@@ -49,3 +49,35 @@
     </div>
   </div>
 </div>
+<div id="blueimp-gallery" class="blueimp-gallery blueimp-gallery-controls" data-use-bootstrap-modal="false">
+    <!-- The container for the modal slides -->
+    <div class="slides"></div>
+    <h3 class="title"></h3>
+    <a class="prev">‹</a>
+    <a class="next">›</a>
+    <a class="close">×</a>
+    <a class="play-pause"></a>
+    <ol class="indicator"></ol>
+    <!-- The modal dialog, which will be used to wrap the lightbox content -->
+    <div class="modal fade">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <button type="button" class="close" aria-hidden="true">&times;</button>
+            <h4 class="modal-title"></h4>
+          </div>
+          <div class="modal-body next"></div>
+            <div class="modal-footer">
+            <button type="button" class="btn btn-default pull-left prev">
+              <i class="fa fa-arrow-left"></i>
+              <?php print t('Previous'); ?>
+            </button>
+            <button type="button" class="btn btn-primary next">
+              <?php print t('Next'); ?>
+              <i class="fa fa-arrow-right"></i>
+            </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
--- templates/culturefeed-uitpas-promotion.tpl.php
+++ templates/culturefeed-uitpas-promotion.tpl.php
@@ -51,3 +51,35 @@
     </div>
   </div>
 </div>
+<div id="blueimp-gallery" class="blueimp-gallery blueimp-gallery-controls" data-use-bootstrap-modal="false">
+    <!-- The container for the modal slides -->
+    <div class="slides"></div>
+    <h3 class="title"></h3>
+    <a class="prev">‹</a>
+    <a class="next">›</a>
+    <a class="close">×</a>
+    <a class="play-pause"></a>
+    <ol class="indicator"></ol>
+    <!-- The modal dialog, which will be used to wrap the lightbox content -->
+    <div class="modal fade">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <button type="button" class="close" aria-hidden="true">&times;</button>
+            <h4 class="modal-title"></h4>
+          </div>
+          <div class="modal-body next"></div>
+            <div class="modal-footer">
+            <button type="button" class="btn btn-default pull-left prev">
+              <i class="fa fa-arrow-left"></i>
+              <?php print t('Previous'); ?>
+            </button>
+            <button type="button" class="btn btn-primary next">
+              <?php print t('Next'); ?>
+              <i class="fa fa-arrow-right"></i>
+            </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
