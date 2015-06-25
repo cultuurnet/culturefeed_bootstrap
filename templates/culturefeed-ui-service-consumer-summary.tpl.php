79aca000545c07f788adc1beecbaf22a785aa9d0
Julie Vanopdenbosch <julievanopdenbosch@Julies-MacBook-Air.local>
2015-06-25 15:51:59 +0200

    MSS-182 UiV3: Aanpassen UiTID opkuisen verkeerd aangepaste tpls & preprocess

---
+++ templates/culturefeed-ui-service-consumer-summary.tpl.php
@@ -0,0 +1,6 @@
+<div class="row">
+  <div class="col-xs-12"><i class="fa fa-link"></i>&nbsp;<?php print $name ?></div>
+  <!--<div class="col-sm-4 small"><?php print $logo ?></div>-->
+</div>
+<div class="col-xs-12 date"><?php print $creation_date ?></div>
+<div class="col-xs-12"><?php print $revoke_link ?></div>
