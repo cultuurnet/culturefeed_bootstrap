8f5156b117c38e5b4b60d7d2e4f2d6f86c0031d4
Julie Vanopdenbosch <julievanopdenbosch@Julies-MacBook-Air.local>
2015-06-25 09:05:22 +0200

    MSS-201 Proper maken accounts op login data: online accounts

--- templates/culturefeed-ui-online-account.tpl.php
+++ templates/culturefeed-ui-online-account.tpl.php
@@ -1,24 +1,21 @@
-
-  <!--icon-->
-  <?php if ($type == 'facebook') : ?>
-    <i class="fa fa-facebook-square"></i> 
-    <?php elseif ($type == 'twitter'): ?>
-    <i class="fa fa-twitter-square"></i> 
-    <?php elseif ($type == 'google'): ?>
-    <i class="fa fa-google-plus-square"></i> 
-  <?php endif; ?>
-  <!--link-->
-  <?php if ($connect_link && !$delete_link) : ?>
-  <?php print $connect_link ?>
-  <?php endif; ?>
-  <?php if ($delete_link) : ?>
-  <?php print $delete_link ?>
-  <?php endif; ?>
-  <!--data-->
+  <div class="col-xs-12">
+    <?php if ($type == 'facebook') : ?>
+      <i class="fa fa-facebook-square"></i> 
+      <?php elseif ($type == 'twitter'): ?>
+      <i class="fa fa-twitter-square"></i> 
+      <?php elseif ($type == 'google'): ?>
+      <i class="fa fa-google-plus-square"></i> 
+    <?php endif; ?>
+    <?php if ($connect_link && !$delete_link) : ?>
+    <?php print $connect_link ?>
+    <?php endif; ?>
+    <?php if ($delete_link) : ?>
+    <?php print $delete_link ?>
+    <?php endif; ?>
+  </div>
   <?php if ($picture || $name || $nick || $publish_link) : ?>
-  <div class="user-account-actions">
-    
-    <div class="profile-small">
+  <div class="inside clearfix">
+    <div class="col-sm-4 user-accounts--profile">
       <?php if ($picture) : ?>
         <?php print $picture ?>
       <?php endif; ?>
@@ -29,12 +26,11 @@
         <?php print $nick ?>
       <?php endif; ?>
     </div>
-    
-    <!--privacy-->
-    <?php if ($publish_link) : ?>
-      <?php print $publish_link ?>
-    <?php endif; ?>
-  
+    <div class="col-sm-8 user-accounts--privacy">
+      <?php if ($publish_link) : ?>
+        <?php print $publish_link ?>
+      <?php endif; ?>
+    </div>  
   </div>
   <?php endif; ?>
   
