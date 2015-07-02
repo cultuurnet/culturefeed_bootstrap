1a2c738914c06466b9e98c2c3152486fd2c34ae5
Julie Vanopdenbosch <julievanopdenbosch@Julies-MacBook-Air.local>
2015-07-02 14:47:57 +0200

    MSS-186 UiV3: Aanpassen UiTpas voordelen sidebar blocks

---
+++ templates/culturefeed-uitpas-profile-section.tpl.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * @file
+ * Contains culturefeed-uitpas-profile-section.tpl.php.
+ */
+
+?>
+
+<div class="points clearfix">
+  <label><?php print $points_label; ?>:</label>
+  <span class="label label-primary pull-right"><?php print $points_value; ?></span>
+</div>
+<div class="links">
+  <?php print $links; ?>
+</div>
