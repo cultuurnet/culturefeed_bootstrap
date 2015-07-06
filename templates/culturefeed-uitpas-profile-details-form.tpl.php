bcf8a5f1865b29e21180978a72d02a6bf8782d3d
HnLn <hans.langouche@gmail.com>
2015-07-06 11:10:44 +0200

    MSS-169 dpm fix

--- templates/culturefeed-uitpas-profile-details-form.tpl.php
+++ templates/culturefeed-uitpas-profile-details-form.tpl.php
@@ -5,8 +5,6 @@
  * Default theme implementation to display culturefeed uitpas profile details form.
  */
 
-dpm(get_defined_vars());
-
 ?>
   <div class="row">
     <div class="col-xs-6"><?php print $first_name; ?></div>
