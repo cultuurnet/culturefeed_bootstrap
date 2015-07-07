2a6f0b67d8e8f8bbc242373b78d0398802970144
Julie Vanopdenbosch <julievanopdenbosch@Julies-MacBook-Air.local>
2015-07-07 10:25:06 +0200

    MSS-184 form details responsive classes

--- templates/culturefeed-uitpas-profile-details-form.tpl.php
+++ templates/culturefeed-uitpas-profile-details-form.tpl.php
@@ -7,33 +7,33 @@
 
 ?>
   <div class="row">
-    <div class="col-xs-6"><?php print $first_name; ?></div>
-    <div class="col-xs-6"><?php print $last_name; ?></div>
+    <div class="col-sm-6"><?php print $first_name; ?></div>
+    <div class="col-sm-6"><?php print $last_name; ?></div>
   </div>
   <div class="row">
-    <div class="col-xs-6"><?php print $dob; ?></div>
-    <div class="col-xs-6"><?php print $pob; ?></div>
+    <div class="col-sm-6"><?php print $dob; ?></div>
+    <div class="col-sm-6"><?php print $pob; ?></div>
   </div>
   <div class="row">
-    <div class="col-xs-6"><?php print $gender; ?></div>
-    <div class="col-xs-6"><?php print $nationality; ?></div>
+    <div class="col-sm-6"><?php print $gender; ?></div>
+    <div class="col-sm-6"><?php print $nationality; ?></div>
   </div>
   <div class="row">
-    <div class="col-xs-8"><?php print $street; ?></div>
-    <div class="col-xs-4"><?php print $nr; ?></div>
+    <div class="col-sm-8"><?php print $street; ?></div>
+    <div class="col-sm-4"><?php print $nr; ?></div>
   </div>
   <div class="row">
-    <div class="col-xs-4"><?php print $zip; ?></div>
-    <div class="col-xs-8"><?php print $city; ?></div>
+    <div class="col-sm-4"><?php print $zip; ?></div>
+    <div class="col-sm-8"><?php print $city; ?></div>
   </div>
   <div class="row">
-    <div class="col-xs-6"><?php print $tel; ?></div>
-    <div class="col-xs-6"><?php print $mobile; ?></div>
+    <div class="col-sm-6"><?php print $tel; ?></div>
+    <div class="col-sm-6"><?php print $mobile; ?></div>
   </div>
   <div class="row">
-    <div class="col-xs-6"><?php print $email; ?></div>
-    <div class="col-xs-6"><?php print $email_description; ?></div>
+    <div class="col-sm-6"><?php print $email; ?></div>
+    <div class="col-sm-6"><?php print $email_description; ?></div>
   </div>
   <div class="row">
-    <div class="col-xs-12"><?php print $main_form; ?><?php print $actions; ?></div>
+    <div class="col-sm-12"><?php print $main_form; ?><?php print $actions; ?></div>
   </div>
