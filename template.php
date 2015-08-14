09cc3a8adda8adbe5e9814d8ceaa41570d63bd68
zuuperman <nils.destoop@gmail.com>
2015-08-14 11:49:59 +0200

    Hotfix: Drupal triggering watchdog warnings when culturefeed_social was disabled. The theme hooks where not found. Added a check if the module is enabled

--- template.php
+++ template.php
@@ -161,7 +161,7 @@ function _culturefeed_bootstrap_preprocess_culturefeed_agenda(&$variables) {
   $item = $variables['item'];
   $entity = $item->getEntity();
 
-  if (!culturefeed_is_culturefeed_user()) {
+  if (!culturefeed_is_culturefeed_user() && module_exists('culturefeed_social')) {
     $variables['recommend_link'] = theme('culturefeed_social_login_required_message', array(
       'activity_type' => CultureFeed_Activity::TYPE_RECOMMEND,
       'item' => $item,
