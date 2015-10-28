3134d0f0153c6f023c1e2519583a2f864121b69d
Julie Vanopdenbosch <julievanopdenbosch@Julies-MacBook-Air.local>
2015-10-28 15:34:31 +0100

    MSS-180 UiV3: Aanpassen Mijn UiTpas voordelen overzichtspagina: filters advantages & promotions

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -129,7 +129,7 @@
   <div class="row">
 
     <?php if (!empty($page['sidebar_first'])): ?>
-      <aside class="col-md-3" role="complementary">
+      <aside class="col-sm-4 col-md-3" role="complementary">
         <?php print render($page['sidebar_first']); ?>
       </aside>  <!-- /#sidebar-first -->
     <?php endif; ?>
@@ -158,7 +158,7 @@
     </section>
 
     <?php if (!empty($page['sidebar_second'])): ?>
-      <aside class="col-md-3" role="complementary">
+      <aside class="col-sm-4 col-md-3" role="complementary">
         <?php print render($page['sidebar_second']); ?>
       </aside>  <!-- /#sidebar-second -->
     <?php endif; ?>
