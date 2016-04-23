673af8d1efcaa62a0f0933987b27f7690896f48e
Luk Dens <luk.dens@cultuurnet.be>
2016-04-23 20:21:41 +0200

    override templates for culturefeed mailing admin page

---
+++ templates/culturefeed-mailing-user-profile-admin-item.tpl.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * @file
+ * Template for an item in the management of your mailings.
+ */
+?>
+
+<p class="lead"><strong><?php print $title; ?></strong></p>
+<p class="text-muted"><?php print $description; ?></p>
+<p><?php print $optin_toggle; ?></p>
+<hr />
