493ea008142a87f149ca2834624796d658484cc8
Luk Dens <luk.dens@cultuurnet.be>
2016-05-06 11:28:30 +0200

    UIV-1765 fix responsive issues caused by missing viewport attributes

--- templates/html.tpl.php
+++ templates/html.tpl.php
@@ -36,6 +36,7 @@
  * - $classes String of classes that can be used to style contextually through
  *   CSS.
  *
+ * @see bootstrap_preprocess_html()
  * @see template_preprocess()
  * @see template_preprocess_html()
  * @see template_process()
@@ -47,9 +48,15 @@
 <html lang="<?php print $language->language; ?>" dir="<?php print $language->dir; ?>"<?php print $rdf_namespaces; ?>>
 
 <head profile="<?php print $grddl_profile; ?>">
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <?php print $head; ?>
   <title><?php print $head_title; ?></title>
   <?php print $styles; ?>
+  <!-- HTML5 element support for IE6-8 -->
+  <!--[if lt IE 9]>
+    <script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+  <![endif]-->
   <?php print $scripts; ?>
 </head>
 <body class="<?php print $classes; ?>" <?php print $attributes;?>>
