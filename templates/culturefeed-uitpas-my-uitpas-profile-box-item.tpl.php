cf85b47ff47911ef8d19009039ef0501419f8e6e
Luk Dens <luk.dens@cultuurnet.be>
2017-08-25 15:45:04 +0200

    CF-481 pull the points label to the right

--- templates/culturefeed-uitpas-my-uitpas-profile-box-item.tpl.php
+++ templates/culturefeed-uitpas-my-uitpas-profile-box-item.tpl.php
@@ -1,4 +1,4 @@
 <a href="<?php print $path; ?>">
   <?php print t('My UiTPAS'); ?>
-  <span class="label label-default"><?php print $points; ?> ptn</span>
+  <span class="label label-default pull-right"><?php print $points; ?> ptn</span>
 </a>
