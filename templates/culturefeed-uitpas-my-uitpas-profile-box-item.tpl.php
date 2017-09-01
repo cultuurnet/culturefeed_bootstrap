425d5495177663138d0594b6c2d7b7260697258e
Stijn Swaanen <swaanenstijn@gmail.com>
2017-09-01 15:08:19 +0200

    CF-481 use new url parameter instead of path

--- templates/culturefeed-uitpas-my-uitpas-profile-box-item.tpl.php
+++ templates/culturefeed-uitpas-my-uitpas-profile-box-item.tpl.php
@@ -1,4 +1,4 @@
-<a href="<?php print $path; ?>">
+<a href="<?php print $url; ?>">
   <?php print t('My UiTPAS'); ?>
   <span class="label label-default pull-right"><?php print $points; ?> ptn</span>
 </a>
